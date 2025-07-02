a0f99362f55e97e6c18718cb2a5d6c799eb36c9e
Евгения <ev.sherbacov@yandex.ru>
2025-07-02 14:37:50 +0300

    lesson2

--- 02 - lesson/Lesson_2_task_2.py
+++ 02 - lesson/Lesson_2_task_2.py
@@ -1,12 +1,11 @@
-def    is_year_leap(year):
+def is_year_leap(year):
     if year % 4 == 0:
 
 
-      return ("True")
+      return True
     else:
-      return ("False")
+      return False
 
 year = 2020
 leap_year = is_year_leap(year)
-print(f"год {year}: {leap_year}")
 print(f"год {year}: {leap_year}")

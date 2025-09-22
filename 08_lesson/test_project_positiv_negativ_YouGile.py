f9a99d2c9a159accc4b3cfc057fdb0aa509d8c4c
Евгения <ev.sherbacov@yandex.ru>
2025-09-22 11:07:37 +0300

    08_Lesson

--- 08_lesson/test_project_positiv_negativ_YouGile.py
+++ 08_lesson/test_project_positiv_negativ_YouGile.py
@@ -6,11 +6,8 @@ COMPANY_ID = "65797906-9c89-406b-a6b3-480d0e5206a5"
 
 api = ProjectYouGile('https://ru.yougile.com/api-v2/', LOGIN, PASSWORD, COMPANY_ID)
 
-def test_create_project_():
-    # количество проектов до
-    projects_before = api.get_project_list()
-    len_before = len(projects_before)
 
+def test_create_project_positive():
     # создание проекта
     title = 'ГосУслуги'
     users = {
@@ -18,19 +15,15 @@ def test_create_project_():
     }
 
     result = api.create_project(title, users)
-    new_id = result.json()['id']
 
     # количество проектов после
     projects_after = api.get_project_list()
-    len_after = len(projects_after)
 
     assert result.status_code == 201
-    assert len_after - len_before == 1
     assert projects_after[-1]['title'] == title
-    assert projects_after[-1]['id'] == new_id
 
 
-def test_get_project_with_id():
+def test_get_project_with_id_positive():
     # создание проекта
     title = 'ГосУслуги'
     users = {
@@ -46,7 +39,7 @@ def test_get_project_with_id():
     assert new_project.json()['users'] == users
 
 
-def test_edit_project():
+def test_edit_project_positive():
     title = 'Edit_ГосУслуги'
     users = {
         "62d0c983-8ffe-440f-b07a-700b31a2ba83": "admin"
@@ -55,7 +48,7 @@ def test_edit_project():
 
     project_id = result.json()['id']
     new_deleted = True
-    new_title = 'uhiuГосУслуги'
+    new_title = 'Edited_ГосУслуги'
 
     edited = api.edit_project(project_id, new_deleted, new_title)
     new_project = api.get_project_with_id(project_id)
@@ -64,5 +57,49 @@ def test_edit_project():
     assert new_project.json()['title'] == new_title
 
 
+def test_create_project_negative():
+    # создание проекта
+    title = ' '
+    users = {
+        "62d0c983-8ffe-440f-b07a-700b31a2ba83": "admin"
+    }
 
+    result = api.create_project(title, users)
+    project_id = result.json()['id']
+
+    assert result.status_code == 201
+    assert project_id in result.text
+
+
+def test_get_project_with_id_negative():
+    title = 'ГосУслуги'
+    users = {
+        "62d0c983-8ffe-440f-b07a-700b31a2ba83": "admin"
+    }
+    result = api.create_project(title, users)
+    project_id = result.json()['id']
+
+    # обращаемся к проекту
+    new_project = api.get_project_with_id(project_id)
+
+    assert new_project.json()['title'] == title
+    assert new_project.json()['users'] == users
+
+
+def test_edit_project_negative():
+    title = 'gltguyigtliuy'
+    users = {
+        "62d0c983-8ffe-440f-b07a-700b31a2ba83": "admin"
+    }
+    result = api.create_project(title, users)
+
+    project_id = result.json()['id']
+    new_deleted = True
+    new_title = 'Edited_ГосУслуги'
+
+    edited = api.edit_project(project_id, new_deleted, new_title)
+    new_project = api.get_project_with_id(project_id)
+
+    assert edited.status_code == 200
+    assert new_project.json()['title'] == new_title
 
